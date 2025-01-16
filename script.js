34103afe8f1e1f4c5c2455ef0249384b66520757
urmilarawat005 <u4urmilarawat@gmail.com>
2025-01-16 15:48:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+  function updateUrl() {
+            const name = document.getElementById("name").value;
+            const year = document.getElementById("year").value;
+            
+            let baseUrl = "https://localhost:8080/";
+            let queryString = "";
+
+            if (name) {
+                queryString += "name=" + encodeURIComponent(name);
+            }
+
+            if (year) {
+                if (queryString) {
+                    queryString += "&";
+                }
+                queryString += "year=" + encodeURIComponent(year);
+            }
+
+            document.getElementById("url").textContent = baseUrl + (queryString ? "?" + queryString : "");
+        }
